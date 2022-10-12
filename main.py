e8dbb878ce1eec53aa63d8245f3af038722fa6dd
Melina <101183451+MeliMaidana@users.noreply.github.com>
2022-10-12 13:11:11 +0000

    funciones checkbox perro

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
-from flask import Flask, render_template, request, jsonify, session
-import sqlite3
+from flask import Flask, render_template, request, jsonify, session , current_app
+import sqlite3, os
+from os.path import abspath, dirname, join
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = './media'
+ALLOWED_EXTENSION = {'png', 'jpg', 'jpeg'}
+path = './media'
+path2 = 'img'
 
 app = Flask(__name__)
 
+# Define the application directory
+# BASE_DIR = dirname(dirname(abspath(__file__)))
+# Media dir
+# MEDIA_DIR = join(BASE_DIR, 'media')
+# POSTS_IMAGES_DIR = join(MEDIA_DIR, 'posts')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'esto-es-una-clave-muy-secreta'
+
 
 @app.route('/')
 def index():
-    return render_template('probandoFormulario.html')
+    return render_template('probandoPerfiles.html')
 
 @app.route('/celes')
 def celes():
     return render_template('home.html')
 @app.route('/celes2')
 def Inicio():
-    return render_template('Inicio.html')
+    return render_template('inicio.html')
 @app.route('/celes3')
-def AdoptaGatos():
-    return render_template('AdoptaGatos.html')
+def mision():
+    return render_template('mision.html')
 @app.route('/celes4')
+def AdoptaGatos():
+    return render_template('AdoptaGatos2.html')
+@app.route('/celes5')
 def AdoptaPerros():
     return render_template('AdoptaPerros.html')
-@app.route('/celes5')
+@app.route('/celes6')
 def Refugio():
     return render_template('Refugio.html')
-    
+
 
 @app.route('/meli')
 def meli():
@@ -52,13 +69,26 @@ def meli2():
 def cargarPerfiles():
   if (request.method == "POST"):
     nombre = request.form["nombre"]
-    sexo = request.form["sexo"]
-    edad = request.form["edad"]
-    raza = request.form["raza"]
-    tamaño = request.form["tamaño"]
-    informacion = request.form["informacion"]
-    imagen = request.form["imagen"]
-    print(nombre)
+    #imagen = request.form["imagen"]
+    file = request.files['imagen']
+
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+    print(file_path)
+
+    if request.form.getlist('perro')[0] == 'perro':
+      print('Perro on')
+    elif request.form.getlist('gato')[1] == 'gato':
+      print('Perro off')
+        #el problema es si seleccionamos sólo gato.
+
+      
+    #conn = sqlite3.connect('tabla.db')
+
+
+
+    
     return render_template('base.html')
 
 @app.route('/saludo', methods=["GET", "POST"])
@@ -91,5 +121,31 @@ def borrarPerfil():
   return render_template('borrarPerfil.html')
 
 """
+ sexo = request.form["sexo"]
+    edad = request.form["edad"]
+    raza = request.form["raza"]
+    tamaño = request.form["tamaño"]
+    informacion = request.form["informacion"]
+
+
+
+    q = fSELECT imagen FROM Gatos WHERE imagen = '{file_path}'
+    resu = conn.execute(q)
+    p = fSELECT imagen FROM Perros WHERE imagen = '{file_path}'
+    resuP = conn.execute(p)
+
+    if resu.fetchone():
+      print('Por favor renombre el archivo, el anterior ya existe.')
+      return render_template('probandoPerfiles.html')
+    elif resuP.fetchone():
+      print('Por favor renombre el archivo, el anterior ya existe.')
+      return render_template('probandoPerfiles.html')
+    else:  
+      r = f INSERT INTO Perros (imagen) VALUES ('{file_path}');
+      conn.execute(r)
+      conn.commit()
+      conn.close()
 """
 app.run(host='0.0.0.0', port=81)
+
+
